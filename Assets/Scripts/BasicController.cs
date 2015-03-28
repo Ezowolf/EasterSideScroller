5045ab79c73bbb5549e44e3149ec2af85945d823
Sebastiaan <wiggler@live.nl>
2015-03-28 22:21:52 +0100

    Added The Game
    C# Game

---
+++ Assets/Scripts/BasicController.cs
@@ -0,0 +1,167 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BasicController : MonoBehaviour {
+	private Vector2 movement;
+
+	public AudioClip moneySound;
+	public AudioClip jumpSound;
+	
+    public float walkSpeed = 2f;
+	public float jumpForce = 200f;
+	
+	private bool m_FacingRight = true; 
+	private bool is_Grounded = true;
+	private bool is_Walking = false;
+
+	public int totalCoins;
+
+	public Text coinText;
+	public Text gameOverText;
+
+	private Rigidbody2D m_Rigidbody2D;
+	private Animator animator;
+	private AudioSource source;
+	private ParallaxController _parallaxController;
+
+
+	void Start () {
+		GetTheComponents();
+		gameOverText.text = "";
+		totalCoins = 0;
+		coinText.text = "Coins: 0";
+	}
+
+	void Update () {
+		ButtonBasedMovement();
+		WalkChecker();
+		IdlerChecker();
+	}
+
+	void FixedUpdate(){
+		VelocityChecker();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Coin")
+		{
+			source.PlayOneShot(moneySound);
+			totalCoins = totalCoins + 1;
+			Destroy(other.gameObject);
+			coinText.text = "Coins: "+totalCoins;
+		}
+		
+		if(other.gameObject.tag == "End")
+		{
+			source.PlayOneShot(moneySound);
+			Destroy(other.gameObject);
+			gameOverText.text = "End of test level. Congrats, you saved easter! :D Press R to Restart.";
+		}
+	}
+	private void GetTheComponents()
+	{
+		animator = GetComponent<Animator>();
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+		_parallaxController = GetComponent<ParallaxController> ();
+		source = GetComponent<AudioSource>();
+
+	}
+
+	private void ButtonBasedMovement()
+	{
+		if (Input.GetKey("a"))
+		{
+			if(is_Grounded == true)
+			{
+			animator.SetInteger("state",1);
+			}
+			if(m_FacingRight == true)
+			{
+				Flip();
+			}
+			transform.Translate(-Vector3.right * (walkSpeed+totalCoins/33) * Time.deltaTime);
+			is_Walking = true;
+			_parallaxController.Scroll (1* new Vector2(1f,0f));
+		}
+
+		if (Input.GetKey("d"))
+		{
+			if(is_Grounded == true)
+			{
+			animator.SetInteger("state",1);
+			}
+			if(m_FacingRight == false)
+			{
+				Flip();
+			}
+			transform.Translate(Vector3.right * (walkSpeed+totalCoins/33) * Time.deltaTime);
+			is_Walking = true;
+			_parallaxController.Scroll (1*new Vector2(-1f,0f));
+		}
+		if (is_Grounded == true && Input.GetKeyDown("space"))
+		{
+			source.PlayOneShot(jumpSound);
+			m_Rigidbody2D.AddForce(new Vector2(0f, jumpForce+totalCoins*2));
+		}
+		if (Input.GetKeyUp("r"))
+		{
+			Application.LoadLevel (Application.loadedLevelName);
+		}
+
+	}
+	private void IdlerChecker()
+	{
+		if( is_Walking == false && is_Grounded == true &&!Input.GetKey("a") &&! Input.GetKey("d") )
+		{
+			animator.SetInteger("state",0);
+		}
+	}
+	
+	private void WalkChecker()
+	{
+		if (Input.GetKeyUp("a") && is_Grounded == true)
+		{
+			is_Walking = false;
+		}
+		
+		if (Input.GetKeyUp("d" )&& is_Grounded == true)
+		{
+			is_Walking = false;
+		}
+	}
+	
+	private void VelocityChecker()
+	{
+		if(m_Rigidbody2D.velocity.y == 0 && is_Grounded == false)
+		{
+			is_Walking = false;
+			is_Grounded = true;
+		}
+		if(m_Rigidbody2D.velocity.y > 0)
+		{
+			is_Grounded = false;
+			animator.SetInteger("state",2);
+		}
+		if(m_Rigidbody2D.velocity.y < 0)
+		{
+			is_Grounded = false;
+			animator.SetInteger("state",3);
+		}
+	}
+
+	private void Flip()
+	{
+		m_FacingRight = !m_FacingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+	void OnDestroy()
+	{
+		gameOverText.text = "GAME OVER :( ";
+		Application.LoadLevel (Application.loadedLevelName);
+	}
+}
---
+++ Assets/Scripts/EnemyOne.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+public class EnemyOne : MonoBehaviour {
+	public float moveSpeed = 1.0f;
+	public Vector2 moveAmount;
+	private float moveDirection = 1.0f;
+	public GameObject playerObj;
+	private Rigidbody2D p_rigibody;	
+	private AudioSource source;	
+	public AudioClip plopSound;
+	public GameObject coin;
+	public GameObject splat;
+
+
+	void Start()
+	{
+		source = GetComponent<AudioSource>();
+	}
+
+	void FixedUpdate()
+	{
+		moveAmount.x = moveDirection * moveSpeed * Time.deltaTime;
+		transform.Translate(moveAmount);
+	}
+	
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if(other.gameObject.name != "Player")
+		{
+		Flip();
+		}
+		else
+		{
+			source.PlayOneShot(plopSound);
+			p_rigibody = playerObj.GetComponent<Rigidbody2D>();
+		if(p_rigibody.velocity.y<0)
+			{
+				Instantiate(splat, transform.position, transform.rotation);
+				Instantiate(coin, transform.position, transform.rotation);
+				AudioSource.PlayClipAtPoint(plopSound, transform.position);
+				p_rigibody.AddForce(new Vector2(0f, 500f));
+				Destroy(this.gameObject);
+			}
+			else
+			{
+				Destroy(other.gameObject);
+			}
+		}
+		
+	}
+	private void Flip()
+	{
+		moveDirection *= -1;
+		Vector3 enemyScale = transform.localScale;
+		enemyScale.x *= -1;
+		transform.localScale = enemyScale;
+	}
+}
